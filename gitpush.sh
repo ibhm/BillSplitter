dd875008ca9dc7005f17fd7f1567365dc6107f1f
Malik Ibrahim <maliki2@mcmaster.ca>
2019-09-24 11:35:45 -0400

    Test

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#@Author Ibrahim Malik
+#This is an automated bash script run on command line to update a git repo
+#To use, enter sudo ./gitpush.sh in command line 
+##OR USE sh "path to filename" in command line
+#Might have to use chmod +x gitpush.sh initially or enter a password
+
+git add *
+
+git commit -m "Update"
+
+git push origin master
